e8baa436a55595c1dce79918545e4d3c36c70119
Ben Pski <ben@pepski.com>
2024-01-14 13:51:49 -0600

    can delete nodes, and then edges and sets will respond

--- static/index.js
+++ static/index.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('drawing');
 const context = canvas.getContext('2d');
+const mode_display = document.getElementById('mode_display');
+var mode = null;
 
 const NODE_RADIUS = 5;
 const HEIGHT = 800;
@@ -12,18 +14,28 @@ class Graph {
     constructor() {
         this.nodes = {};
         this.edges = {};
+        this.sets = {};
     }
 
     addNode(node) {
         if (!(node.name in this.nodes)) {
             this.nodes[node.name] = node;
+            node.addDelete((node_name) => {
+                console.log("deleting a node from graph");
+                delete this.nodes[node_name];
+            });
         }
         return this;
     }
 
     createEdge(from, to) {
         if (!(from.name in this.nodes && to.name in this.nodes && [from.name, to.name] in this.edges)) {
-            this.edges[[from.name, to.name]] = new Edge(from, to);
+            var edge = new Edge(from, to);
+            this.edges[[from.name, to.name]] = edge;
+            edge.addDelete((edge_name) => {
+                console.log("deleting an edge from graph");
+                delete this.edges[edge_name]
+            });
         }
         return this;
     }
@@ -31,8 +43,22 @@ class Graph {
     addEdge(edge) {
         if (!(edge.from.name in this.nodes && edge.to.name in this.nodes && [edge.from.name, edge.to.name] in this.edges)) {
             this.edges[[edge.from.name, edge.to.name]] = edge;
+            edge.addDelete((edge_name) => {
+                console.log("deleting an edge from graph");
+                delete this.edges[edge_name];
+            });
         }
         return this;
+    }
+
+    addSet(set) {
+        if (!(set.name in this.sets)) {
+            this.sets[set.name] = set;
+            set.addDelete((set_name) => {
+                console.log("deleting a set from graph");
+                delete this.sets[set_name];
+            });
+        }
     }
 
     closestNode(x, y) {
@@ -61,6 +87,11 @@ class Graph {
             var val = this.edges[key];
             val.draw(canvas);
         }
+
+        for (var key in this.sets) {
+            var val = this.sets[key];
+            val.draw(canvas);
+        }
     }
 }
 
@@ -70,6 +101,7 @@ class Node {
         this.x = x;
         this.y = y;
         this.info = info;
+        this.delete_callbacks = [];
     }
 
     // is (x,y) within the node
@@ -92,13 +124,33 @@ class Node {
             canvas.stroke();
         }
     }
+
+    addDelete(func) {
+        this.delete_callbacks.push(func);
+    }
+
+    delete() {
+        console.log("delete event for node: ", this.name);
+        for (var callback of this.delete_callbacks) {
+            callback(this.name);
+        }
+    }
 }
 
-class Edge {
+class Edge extends EventTarget {
     constructor(from, to) {
+        super();
         this.from = from;
         this.to = to;
         this.info = {};
+        this.delete_callbacks = [];
+        this.from.addDelete((node_name) => {
+            // if any node is deleted then the edge is deleted
+            this.delete();
+        });
+        this.to.addDelete((node_name) => {
+            this.delete();
+        });
     }
 
     draw(canvas) {
@@ -151,16 +203,37 @@ class Edge {
         canvas.closePath();
         canvas.fill();
     }
+
+    addDelete(func) {
+        this.delete_callbacks.push(func);
+    }
+
+    delete() {
+        const key = [this.from.name, this.to.name];
+        console.log("delete event for edge: ", key);
+        for (var callback of this.delete_callbacks) {
+            callback(key);
+        }
+    }
 }
 
 class Set {
-    constructor()  {
+    constructor(name)  {
         this.nodes = {};
+        this.name = name;
+        this.delete_callbacks = [];
     }
 
     addNode(node) {
         if (!(node.name in this.nodes)) {
             this.nodes[node.name] = node;
+            node.addDelete((node_name) => {
+                console.log("delete node from set");
+                delete this.nodes[node_name];
+                if (Object.keys(this.nodes).length == 0) {
+                    this.delete()
+                }
+            });
         }
         return this;
     }
@@ -179,7 +252,6 @@ class Set {
             node.draw(canvas)
         }
         var hull = convex_hull(points);
-        var [center_x, center_y] = centroid(hull);
         
         var tangent_points = [];
         for (var i=0; i<hull.length; i++) {
@@ -188,7 +260,7 @@ class Set {
             var x2 = hull[(i+1) % hull.length].x;
             var y2 = hull[(i+1) % hull.length].y;
 
-            var m = 2*NODE_RADIUS/Math.sqrt((x2-x1)**2 + (y2-y1)**2);
+            var m = 3*NODE_RADIUS/Math.sqrt((x2-x1)**2 + (y2-y1)**2);
             var a1 = x1 + (y2-y1)*m;
             var b1 = y1 - (x2-x1)*m;
             var a2 = x2 + (y2-y1)*m;
@@ -198,73 +270,6 @@ class Set {
             tangent_points.push({x: a2, y: b2});
         }
 
-        /*
-        // compute the points on the bumped out edges at the perpendicular bisector
-        var edge_points = [];
-        for (var i=0; i<hull.length; i++) {
-            var x1 = hull[i].x;
-            var y1 = hull[i].y;
-            var x2 = hull[(i+1) % hull.length].x;
-            var y2 = hull[(i+1) % hull.length].y;
-            var s = ((x1-x2)*(center_x-x2)+(y1-y2)*(center_y-y2))/((x1-x2)**2+(y1-y2)**2);
-            var a = x2 + s*(x1-x2);
-            var b = y2 + s*(y1-y2);
-
-            var m = 2*NODE_RADIUS/Math.sqrt((a-center_x)**2+(b-center_y)**2);
-            var x = a + m*(a-center_x);
-            var y = b + m*(b-center_y);
-            edge_points.push({x: x, y: y});
-        }
-
-        // compute the intersection points 
-        var intersection_points = [];
-        for (var i=0; i<hull.length; i++) {
-            var x1 = hull[i].x;
-            var y1 = hull[i].y;
-            var x2 = hull[(i+1) % hull.length].x;
-            var y2 = hull[(i+1) % hull.length].y;
-
-            var a = edge_points[i].x;
-            var b = edge_points[i].y;
-
-            var s = -((y1-center_y)*(center_x-a)+(center_x-x1)*(center_y-b))/((x1-x2)*(center_y-y1)-(center_x-x1)*(y1-y2));
-            var u = a + s*(x1-x2);
-            var v = b + s*(y1-y2);
-
-            intersection_points.push({x: u, y: v});
-        }
-        */
-        // debugging drawing
-        /*
-        canvas.beginPath();
-        canvas.arc(center_x, center_y, NODE_RADIUS, 0, Math.PI*2);
-        canvas.stroke();
-        for (const point of tangent_points) {
-            canvas.beginPath();
-            canvas.arc(point.x, point.y, NODE_RADIUS/2, 0, 2*Math.PI);
-            canvas.stroke();
-        }
-        */
-        /*
-        for (const point of edge_points) {
-            canvas.beginPath();
-            canvas.arc(point.x, point.y, NODE_RADIUS/2, 0, Math.PI);
-            canvas.stroke();
-        }
-
-        for (const point of intersection_points) {
-            canvas.beginPath();
-            canvas.arc(point.x, point.y, NODE_RADIUS/2, Math.PI, Math.PI*2);
-            canvas.stroke();
-        }
-        */
-        /*
-        for (const point of hull) {
-            canvas.beginPath();
-            canvas.arc(point.x, point.y, NODE_RADIUS*2, 0, Math.PI*2);
-            canvas.stroke();
-        }
-        */
         canvas.beginPath();
         canvas.moveTo(tangent_points[0].x, tangent_points[0].y);
         for (var i=0; i<tangent_points.length; i++) {
@@ -282,24 +287,22 @@ class Set {
 
                 var start = Math.atan2(b1-y1, a1-x1);
                 var end = Math.atan2(b2-y1, a2-x1);
-                canvas.arc(x1,y1,2*NODE_RADIUS,start,end);
+                canvas.arc(x1,y1,3*NODE_RADIUS,start,end);
                 
             }
         }
         canvas.stroke();
-        /*
-        canvas.beginPath();
-        canvas.moveTo(tangent_points[0].x, tangent_points[0].y);
-        for (var i=0; i<tangent_points.length; i++) {
-            var control1 = tangent_points[(i+1) % tangent_points.length];
-            canvas.lineTo(control1.x, control1.y);
+    }
+
+    addDelete(func) {
+        this.delete_callbacks.push(func);
+    }
+
+    delete() {
+        console.log(this.delete_callbacks);
+        for (var callback of this.delete_callbacks) {
+            callback(this.name);
         }
-        //for (var index in control_points.slice(1)) {
-        //    canvas.arcTo(point.x, point.y);
-        //}
-        //canvas.closePath();
-        canvas.stroke();
-        */
     }
 }
 
@@ -357,7 +360,10 @@ function centroid(points) {
 // testing
 
 var graph = new Graph();
-var set = new Set();
+var set = new Set("1");
+var set2 = new Set("2");
+graph.addSet(set);
+graph.addSet(set2);
 
 var nodeA = new Node("A", 10, 10, {});
 var nodeB = new Node("B", 90, 100, {});
@@ -366,9 +372,8 @@ var nodeD = new Node("D", 200, 80, {});
 
 set.addNode(nodeA);
 set.addNode(nodeB);
-set.addNode(nodeC);
-set.addNode(nodeD);
-set.draw(context);
+set2.addNode(nodeC);
+set2.addNode(nodeD);
 
 var selected_node = null;
 var drawing_edge = null;
@@ -380,48 +385,62 @@ graph.addNode(nodeD);
 
 graph.createEdge(nodeA, nodeB);
 
+function deletion_handling(event, x, y) {
+    console.log("trying to delete");
+    var closest_node = graph.closestNode(x, y);
+    console.log(closest_node);
+    console.log(closest_node.touching(x,y));
+    if (closest_node !== null && closest_node.touching(x, y)) {
+        closest_node.delete()
+    }  
+}
+
 canvas.addEventListener('mousedown', function(event) {
     var x = event.offsetX;
     var y = event.offsetY;
-    
-    // ctrl+click => draw an edge
-    // if not near a node create the node
-    // create a "phantom node" that merges with an overlapping node on release if possible
-    if (event.ctrlKey) {
-        var closest_node = graph.closestNode(x, y);
-        if (closest_node !== null && closest_node.touching(x,y)) {
-            var start_node = closest_node;
-        } else {
-            var id = crypto.randomUUID();
-            var start_node = new Node(id, event.offsetX, event.offsetY, {});
-            graph.addNode(start_node);
-        }
-        var id = crypto.randomUUID();
-        var end_node = new Node(id, event.offsetX, event.offsetY, {});
-        graph.addNode(end_node);
-        selected_node = end_node;
-        end_node.info.selected = true;
-        end_node.info.phantom = true;
-        var new_edge = new Edge(start_node, end_node);
-        graph.addEdge(new_edge)
-        new_edge.info.phantom = true;
-        drawing_edge = new_edge;
+
+    if (mode == 'delete') {
+        deletion_handling(event, x, y);
     } else {
-        var closest_node = graph.closestNode(x, y);
-        console.log(closest_node);
-        console.log(closest_node.touching(x,y));
-        if (closest_node !== null && closest_node.touching(x, y)) {
-            selected_node = closest_node;
-            closest_node.info.selected = true;
-        } else {
+        
+        // ctrl+click => draw an edge
+        // if not near a node create the node
+        // create a "phantom node" that merges with an overlapping node on release if possible
+        if (event.ctrlKey) {
+            var closest_node = graph.closestNode(x, y);
+            if (closest_node !== null && closest_node.touching(x,y)) {
+                var start_node = closest_node;
+            } else {
+                var id = crypto.randomUUID();
+                var start_node = new Node(id, event.offsetX, event.offsetY, {});
+                graph.addNode(start_node);
+            }
             var id = crypto.randomUUID();
-            var node = new Node(id, event.offsetX, event.offsetY, {});
-            graph.addNode(node);
-            selected_node = node;
-            node.info.selected = true;
+            var end_node = new Node(id, event.offsetX, event.offsetY, {});
+            graph.addNode(end_node);
+            selected_node = end_node;
+            end_node.info.selected = true;
+            end_node.info.phantom = true;
+            var new_edge = new Edge(start_node, end_node);
+            graph.addEdge(new_edge)
+            new_edge.info.phantom = true;
+            drawing_edge = new_edge;
+        } else {
+            var closest_node = graph.closestNode(x, y);
+            console.log(closest_node);
+            if (closest_node !== null && closest_node.touching(x, y)) {
+                console.log(closest_node.touching(x,y));
+                selected_node = closest_node;
+                closest_node.info.selected = true;
+            } else {
+                var id = crypto.randomUUID();
+                var node = new Node(id, event.offsetX, event.offsetY, {});
+                graph.addNode(node);
+                selected_node = node;
+                node.info.selected = true;
+            }
         }
     }
-
 })
 
 canvas.addEventListener('mousemove', function(event) {
@@ -445,10 +464,25 @@ canvas.addEventListener('mouseup', function(event) {
         drawing_edge.info.phantom = false;
         drawing_edge = null;
     }
-    selected_node.info.selected = false;
-    selected_node = null;
+    if (selected_node !== null) {
+        selected_node.info.selected = false;
+        selected_node = null;
+    }
 })
 
+document.addEventListener('keydown', function(event) {
+    if (event.key == 'd') {
+        mode = 'delete';
+        mode_display.textContent = 'Mode: delete';
+    }
+})
+
+document.addEventListener('keyup', function(event) {
+    if (event.key == 'd') {
+        mode = null;
+        mode_display.textContent = 'Mode: select';
+    }
+})
 
 const clear = (canvas) => {
     canvas.clearRect(0, 0, WIDTH, HEIGHT);
@@ -458,7 +492,6 @@ const clear = (canvas) => {
 const renderLoop = () => {
     clear(context);
     graph.draw(context);
-    set.draw(context);
     requestAnimationFrame(renderLoop)
 }
 
